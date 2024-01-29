d3f9dcacf5c21e5aaa48e26bac0148247a7c0ad5
Dev2vinod <vinodsinghfun@gmail.com>
2024-01-30 01:06:50 +0530

    first use of express

--- index.mjs
+++ index.mjs
@@ -1,18 +1,18 @@
-//  console.log("hello world node js welxome page");
+ console.log("hello world node js welxome page");
 
-//  let username ="haider ali"
+ let username ="haider ali"
 
-//  console.log('username:',username);
+ console.log('username:',username);
 
-import express from 'express'
-const app = express()
-const port = 3000
+// import express from 'express'
+// const app = express()
+// const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app.listen(port, () => {
-  console.log(`node js working in my pc listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`node js working in my pc listening on port ${port}`)
+// })
  
